f0bcdcbe0d22ca4be5133ed1ed9dd50ea889f058
gaole <gaole@umich.edu>
2017-07-12 02:29:08 -0400

    revise the RNN and minor mistake in FFNN

--- RNNversion.py
+++ RNNversion.py
@@ -30,6 +30,9 @@ train_text = [];
 validation_text = [];
 test_text = [];
 
+state_size = 100;
+input_size = 100;
+
 with zipfile.ZipFile('ted_en-20160408.zip', 'r') as z:
     doc = lxml.etree.parse(z.open('ted_en-20160408.xml', 'r'))
 # input_text = '\n'.join(doc.xpath('//content/text()'))
@@ -58,9 +61,9 @@ for i in range(2085):
 
 	label_vec = [0 for x in range(8)];
 	label_vec[whichclass] = 1;
-	if (i < 1585):
+	if (i < 1085):
 		train_label.append(label_vec);
-	elif (i >= 1585 and i <1835):
+	elif (i >= 1085 and i <1835):
 		validation_label.append(label_vec);
 	else:
 		test_label.append(label_vec);
@@ -79,7 +82,7 @@ for sent_str in sentences_strings_ted:
     tokens = re.sub(r"[^a-z0-9]+", " ", sent_str.lower()).split()
     sentences_ted.append(tokens)
 
-model_ted = Word2Vec(sentences_ted, size=100, window=5, min_count=5, workers=4)
+# model_ted = Word2Vec(sentences_ted, size=100, window=5, min_count=5, workers=4)
 
 
 def get_embedding(word):
@@ -88,7 +91,31 @@ def get_embedding(word):
 	else:
 		return [0 for x in range(100)];
 
+with tf.variable_scope("runn_cell"):
+	W = tf.get_variable("W", [input_size+state_size, state_size]);
+	b = tf.get_variable("b", [state_size]);
 
+def rnn_cell(rnn_input, state):
+	with tf.variable_scope("rnn_cell", reuse=True):
+		W = tf.get_Variable("W", [input_size+state_size, state_size]);
+		b = tf.get_variable("b", [state_size]);
+
+	return tf.tanh(tf.matmul(tf.concat([rnn_input, state], 1), W) + b)
+
+
+def get_snetences_matrix_embedding(sentences):
+	return_matirx = [];
+	for lines in sentences:
+		for word in lines:
+			return_matrix.append(get_embedding(word));
+	return return_matrix;
+
+
+
+
+
+max_length = 0;
+max_lines = [];
 for i in range(2085):
 	tmp = re.sub(r'\([^)]*\)', '', rawtext[i]);
 
@@ -101,23 +128,35 @@ for i in range(2085):
 	for sent_str in sentences_strings_ted:
 	    tokens = re.sub(r"[^a-z0-9]+", " ", sent_str.lower()).split()
 	    sentences_ted.append(tokens)
-	sum_embedding = np.asarray([0 for x in range(100)])
-	num = 1
-	for t in range(len(sentences_ted)):
-		for p in range(len(sentences_ted[t])):
-			sum_embedding = sum_embedding + np.asarray(get_embedding(sentences_ted[t][p]));
-			num = num + 1;
-	sum_embedding = np.asarray(sum_embedding);
-	for tt in range(100):
-		sum_embedding[tt] /= float(num);
-	print(sum_embedding);
 
-	if (i < 1585):
-		train_text.append(sum_embedding);
-	elif (i >= 1585 and i <1835):
-		validation_text.append(sum_embedding);
-	else:
-		test_text.append(sum_embedding);
+	for lines in sentences_ted:
+		if (len(lines) > max_length):
+			max_length = len(lines);
+			max_lines = lines;
+
+
+	
+	# X = tf.placeholder(tf.float32, [None, 100]);
+	# y = tf.placeholder(tf)
+
+	# sum_embedding = np.asarray([0 for x in range(100)])
+	# num = 1
+	# for t in range(len(sentences_ted)):
+	# 	for p in range(len(sentences_ted[t])):
+	# 		sum_embedding = sum_embedding + np.asarray(get_embedding(sentences_ted[t][p]));
+	# 		num = num + 1;
+	# sum_embedding = np.asarray(sum_embedding);
+	# for tt in range(100):
+	# 	sum_embedding[tt] /= float(num);
+
+
+
+	# if (i < 1085):
+	# 	train_text.append(sum_embedding);
+	# elif (i >= 1085 and i <1835):
+	# 	validation_text.append(sum_embedding);
+	# else:
+	# 	test_text.append(sum_embedding);
 
 
 train_text = np.asarray(train_text);
@@ -127,6 +166,8 @@ train_label = np.asarray(train_label);
 validation_label = np.asarray(validation_label);
 test_label = np.asarray(test_label);
 
+print(max_length)
+print(max_lines)
 
 
 def get_next_batch(batch_num, batch_start):
@@ -146,6 +187,9 @@ def get_next_batch(batch_num, batch_start):
 		return return_train_text, return_train_label, batch_start;
 
 
+# def get_text_embedding_rnn():
+
+# 	for i in range(len(sentences)):
 
 
 def training_network(hidden_layer_num, step_length, whether_training, whether_dropout):
@@ -218,12 +262,6 @@ training_network(50, 0.003, 1, 0);
 
 
 del doc
-
-
-# i = input_text.find("Hyowon Gweon: See this?")
-# print(input_text[i-20:i+150])
-
-
 
 
 
